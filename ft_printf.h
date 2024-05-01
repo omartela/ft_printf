a672b6c48d7cb070b0e99fe1b412a847c69ab5a3
Oskari Martela <oskari.martela@gmail.com>
2024-05-01 18:11:28 +0300

    Adds modifications to ft_printf.c and ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -10,25 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef FT_PRINTF.H
-# define FT_PRINTF.H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
-# include <unistd.h>
+# include <stdint.h>
+# include "libft/libft.h"
 # include <stdarg.h>
 
-unsigned int	ft_print_char(const char *format, va_list args);
-unsigned int	ft_print_str(const char *format, va_list args);
+unsigned int	ft_print_char(va_list args);
+unsigned int	ft_print_str(va_list args);
 unsigned int	count_digit(int n);
 unsigned int	count_unsdigit(unsigned int n);
 void			ft_putunsnbr_fd(unsigned int n, int fd);
-unsigned int	ft_print_unsnbr(const char *format, va_list args);
-void			ft_puthexnbr_fd(unsigned int n, int fd, char formatter);
+unsigned int	ft_print_unsnbr(va_list args);
+void			ft_puthexnbr_fd(unsigned int n, int fd, char format);
 unsigned int	count_hexnbr(unsigned int hexnbr);
-unsigned int	ft_print_hexadecimal(const char *format, va_list args, char formatter);
-void 			ft_putptr_fd(uintptr_t ptr, int fd);
+unsigned int	ft_print_hexadecimal(va_list args, char format);
+void			ft_putptr_fd(uintptr_t ptr, int fd);
 unsigned int	count_ptr(uintptr_t ptr);
-unsigned int	ft_print_ptr(const char *format, va_list args);
-unsigned int 	ft_format(const char *format, va_list args, unsigned count);
+unsigned int	ft_print_ptr(va_list args);
+unsigned int	ft_format(const char *format, va_list args, unsigned int count);
+int				ft_printf(const char *format, ...);
+unsigned int	ft_print_nbr(va_list args);
 int				ft_printf(const char *format, ...);
 
-# endif
+#endif
